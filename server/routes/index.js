8cec653cdac0748044c9a8d2298e97aaa997005a
Abubakir Myrzaly <best.abuka@gmail.com>
2019-02-24 22:05:19 -0500

    Added login.ejs and register.ejs

--- server/routes/index.js
+++ server/routes/index.js
@@ -23,3 +23,23 @@ router.get('/', (req, res, next) => {
 });
 
 module.exports = router;
+
+
+/*GET - display the Login Page*/
+router.get('/login', (req, res, next) => {
+  res.render('content/index', {
+    title: 'Home',
+    books: ''
+   });
+});
+
+/*POST - processes the Login Page*/
+router.post('/login', (req, res, next) => {
+  res.render('content/index', {});
+});
+
+/*GET - displays the User Registration Page*/
+
+/*POST - processes the User Registration Page*/
+
+/*GET - perform user logout*/
